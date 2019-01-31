90e75541559d09336b11c475fd538c028d7113ff
Steven Phung <S2phung@ucsd.edu>
2019-01-31 11:40:41 -0800

    node.js lab

--- routes/project.js
+++ routes/project.js
@@ -1,3 +1,7 @@
 exports.viewProject = function(req, res) { 
-  res.render('project');
+  var name = req.params.name; 
+  console.log("The project name is: " + name);
+  res.render('project', {
+    'projectName': name
+  });
 };
